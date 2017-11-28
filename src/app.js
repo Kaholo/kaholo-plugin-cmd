f03e3f5206c8a668e4aeebd548814e91e7d1d74b
Roy Talyosef <roy@productionmap.com>
2017-11-28 10:33:29 +0200

    Added image
    Saved the image inside the plugin and changed the url relativly

--- src/app.js
+++ src/app.js
@@ -73,6 +73,8 @@ var functions = {
 function main(argv) {
 	if (argv.length < 3) {
 		console.log('{err: "not enough parameters"}');
+		// Invalid Argument
+		// Either an unknown option was specified, or an option requiring a value was provided without a value.
 		process.exit(9);
 	}
 	var action = JSON.parse(argv[2]);
@@ -81,6 +83,8 @@ function main(argv) {
 		process.exit(0); // Success
 	}, function(err) {
 		console.log("an error occured", err);
+        // Uncaught Fatal Exception
+		// There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
 		process.exit(1); // Failure
 	});
 }
